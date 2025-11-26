20f5848372477b001116d6f3214b762f94b54c64
Rena3001 <verdiyevarana@gmail.com>
2025-11-26 20:29:19 +0400

    user can access

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,57 +2,36 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
+    public function canAccessPanel(\Filament\Panel $panel): bool
+    {
+        return true; // TEST üçün hər user panelə girə bilər
+    }
+
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
-    }
-
-    public function canAccessFilament(): bool
-{
-    return true;
-}
-
-public function canAccessPanel(\Filament\Panel $panel): bool
-    {
-        return true; // TEST üçün hər user panelə girə bilər
     }
 }
